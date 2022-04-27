559742e68bddd91521d67ac83b913e9e1271be1a
MaximKalinchuk <kalinchuk.maxim@mail.ru>
2022-04-27 14:46:07 +0300

    updated View.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './style.css';
-import valid from './valid.js';
-import parser from './parser.js';
+// import onChange from 'on-change';
+import validate from './valid.js';
+// import parser from './parser.js';
+import render from './View';
 
+const state = {
+  links: [],
+  errors: false,
+};
+
+// onChange(state, render(state));
 // Обработчики
 const form = document.querySelector('.rss-form');
 const textContent = form.querySelector('input');
@@ -11,9 +19,11 @@ form.addEventListener('submit', (event) => {
   const formData = new FormData(event.target);
   const value = Object.fromEntries(formData);
 
-  valid(value);
-  parser(value);
+  const validateResult = validate(value);
+  // получаем список ошибок, если ошибки есть = добавляем их в state. Если ошибок нет
+  render(value, validateResult, state);
 
+  console.log(state);
   textContent.focus();
   form.reset();
 });
--- src/valid.js
+++ src/valid.js
@@ -1,36 +1,15 @@
 import { object, string } from 'yup';
-import render from './View.js';
-
-const state = {
-  links: [],
-  haveLink: false,
-  errors: false,
-};
 
 // Валидатор
-const valid = async (url) => {
+const validate = async (url) => { // state не нужен
   const userSchema = object({ url: string().url().nullable() });
 
   try {
-    const parsedUrl = await userSchema.validate(url);
-
-    if (state.links.includes(parsedUrl.url)) {
-      state.haveLink = true;
-      render(state);
-    }
-
-    if (!state.links.includes(parsedUrl.url)) {
-      state.links.push(parsedUrl.url);
-      state.haveLink = false;
-      render(state);
-    }
+    await userSchema.validate(url);
+    return true;
   } catch (error) {
-    state.errors = true;
-    render(state);
-    state.errors = false;
+    return false;
   }
-
-  console.log('STATE:', state);
 };
 
-export default valid;
+export default validate;
